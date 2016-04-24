f363ea7ae5343ea08736e9721172d8af9e03b4c8
kelsey-jiang <kelsey.jiang@airbnb.com>
2016-04-23 17:09:46 -0700

    new data assembly

--- data_assembly.R
+++ data_assembly.R
@@ -13,6 +13,7 @@ Skills_importance <-
   spread(Element.Name, Data.Value)  %>%
   data.frame()
 Skills_importance$dim <- 'Importance'
+colnames(Skills_importance)[-c(1,2)] <- paste0('skills_',colnames(Skills_importance))[-c(1,2)]
 
 Skills_level <- 
   Skills %>%
@@ -21,6 +22,7 @@ Skills_level <-
   spread(Element.Name, Data.Value)  %>%
   data.frame()
 Skills_level$dim <- 'Level'
+colnames(Skills_level)[-c(1,2)] <- paste0('skills_',colnames(Skills_level))[-c(1,2)]
 
 Knowledge_importance <- 
 Knowledge %>%
@@ -28,6 +30,7 @@ Knowledge %>%
   select(O.NET.SOC.Code, Title, Element.Name, Data.Value) %>%
   spread(Element.Name, Data.Value)  %>%
   data.frame()
+colnames(Knowledge_importance)[-c(1,2)] <- paste0('knowledge_',colnames(Knowledge_importance))[-c(1,2)]
 
 Knowledge_level <- 
   Knowledge %>%
@@ -35,6 +38,7 @@ Knowledge_level <-
   select(O.NET.SOC.Code, Title, Element.Name, Data.Value) %>%
   spread(Element.Name, Data.Value)  %>%
   data.frame()
+colnames(Knowledge_level)[-c(1,2)] <- paste0('knowledge_',colnames(Knowledge_level))[-c(1,2)]
 
 Interests_OI <- 
   Interests %>%
@@ -42,6 +46,15 @@ Interests_OI <-
   select(O.NET.SOC.Code, Title, Element.Name, Data.Value) %>%
   spread(Element.Name, Data.Value)  %>%
   data.frame()
+colnames(Interests_OI)[-c(1,2)] <- paste0('interests_',colnames(Interests_OI))[-c(1,2)]
+
+interests_skills_knowledge<-
+Interests_OI  %>% 
+  left_join(Skills_level, by = c('O.NET.SOC.Code' = 'O.NET.SOC.Code', 'Title' = 'Title')) %>%
+  left_join(Knowledge_importance, by = c('O.NET.SOC.Code' = 'O.NET.SOC.Code', 'Title' = 'Title'))  %>%
+  mutate(O.NET.SOC.Code_short = substr(as.character(O.NET.SOC.Code), 1, 7)) 
+
+
 
 #### Output #### 
 write.csv(Skills_importance, file = 'skills_importance.csv')
