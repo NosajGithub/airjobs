e8e7ca111fae9d3e4963cf0ac6c198ee840a7359
kelsey-jiang <kelsey.jiang@airbnb.com>
2016-04-24 00:13:00 -0700

    add function to process user input strings and pass them to the shiny dashboard

---
+++ shiny_input.R
@@ -0,0 +1,7 @@
+##############################################################
+####             Kelsey Jiang 04/24/2016                  #### 
+##############################################################
+library(tidyr)
+library(dplyr)
+#### Shiny User Interface Input Function ####
+database <- read.csv("~/airjobs/reshaped_data/database.csv")
