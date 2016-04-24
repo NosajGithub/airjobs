ed491c76ac2ad54692d6e8bb2807b8624e21cea7
kelsey-jiang <kelsey.jiang@airbnb.com>
2016-04-24 00:00:00 -0700

    add code to infer NA data

--- data_assembly.R
+++ data_assembly.R
@@ -1,5 +1,6 @@
 library(tidyr)
 library(dplyr)
+
 #### Data #### 
 Skills <- read.csv("~/Desktop/BayesHack/Skills.csv")
 Knowledge <- read.csv("~/Desktop/BayesHack/Knowledge.csv")
@@ -8,6 +9,7 @@ geo_salary <- read.csv("~/airjobs/reshaped_data/geo_salary.csv")
   geo_salary <- geo_salary[ , -2]
 education_required_25pctl <- read.csv("~/airjobs/reshaped_data/education_required_25pctl.csv")
   education_required_25pctl <- education_required_25pctl[ , -1]
+  
 ####  Reshape the data #### 
 Skills_importance <- 
   Skills %>%
@@ -52,13 +54,50 @@ Interests_OI <-
   data.frame()
 colnames(Interests_OI)[-c(1,2)] <- paste0('interests_',colnames(Interests_OI))[-c(1,2)]
 
+#### Assemble the database ####
 database <-
 Interests_OI  %>% 
   full_join(Skills_importance, by = c('O.NET.SOC.Code' = 'O.NET.SOC.Code', 'Title' = 'Title')) %>%
   full_join(Knowledge_importance, by = c('O.NET.SOC.Code' = 'O.NET.SOC.Code', 'Title' = 'Title'))  %>%
   full_join(education_required_25pctl, by = c('O.NET.SOC.Code' = 'O.NET.SOC.Code', 'Title' = 'Title')) %>%
   mutate(O.NET.SOC.Code_short = substr(as.character(O.NET.SOC.Code), 1, 7)) %>%
-  left_join(geo_salary, by = c('O.NET.SOC.Code_short' = 'occ.code'))
+  left_join(geo_salary, by = c('O.NET.SOC.Code_short' = 'occ_code'))
+
+#### Process NAs ####
+database[, 9:43] <-
+database %>% 
+  select(matches('skills_'), -skills_dim) %>%
+  mutate_each(funs(mysum = ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  data.frame()
+
+database[ , 'skills_dim'] <- "Importance"
+
+database[, 'education_level_required'] <-
+  database %>% 
+  select(education_level_required) %>%
+  mutate_each(funs(mysum = ifelse(is.na(.), 1, .))) %>% 
+  data.frame()
+
+database[, 'education_level_required_description'] <-
+  database %>% 
+  select(education_level_required) %>%
+  mutate_each(funs(mysum = ifelse(is.na(.), 'Less than a High School Diploma', .))) %>% 
+  data.frame()
+
+colnames(database) <- gsub("[.]", "_", colnames(database))
+colnames(database) <- tolower(colnames(database))
+
+database[, 45:77] <-
+  database %>% 
+  select(matches('knowledge_')) %>%
+  mutate_each(funs(mysum = ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  data.frame()
+
+database[, 183:234] <-
+  database %>% 
+  select(matches('salary_')) %>%
+  mutate_each(funs(mysum = ifelse(is.na(.), salary_us, .)), -salary_us) %>% 
+  data.frame()
 
 #### Output #### 
 #write.csv(Skills_importance, file = 'skills_importance.csv')
